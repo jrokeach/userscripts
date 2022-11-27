67c7856ce36cf5983b3d12a8f0a2b7996d48c701
Jason R. Rokeach <1076569+jrokeach@users.noreply.github.com>
2022-11-27 11:25:55 -0500

    Add Chase-ConfirmAccountFix.user.js

---
+++ scripts/chase.com/Chase-ConfirmAccountFix.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Chase Confirm Account Number Fix
+// @version      0.1
+// @description  Enable pasting of confirmed account number into Chase's website.
+// @author       Jason R. Rokeach
+// @supportURL   https://github.com/jrokeach/userscripts
+// @match        https://*.chase.com/web/auth/dashboard?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chase.com
+// @grant        none
+// ==/UserScript==
+
+// Thanks goweon for this answer:
+// https://stackoverflow.com/a/47406751
+(new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+
+function check(changes, observer) {
+    if(document.querySelector('#confirmedAccountNumber')) {
+        observer.disconnect();
+        var confirm_input = document.querySelector("#confirmedAccountNumber").shadowRoot.querySelector("#confirmedAccountNumber-input")
+        confirm_input.removeAttribute("onpaste")
+        confirm_input.removeAttribute("autocomplete")
+    }
+}
